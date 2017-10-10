07f1fb1a0cf1f837364b0a493e142e780be5789b
Armen Zambrano G <armenzg@gmail.com>
2017-10-10 15:37:45 -0400

    Use telemetry wrapper to plot evolution within React component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,33 @@
 import React, { Component } from 'react';
 
+const plot = {
+  "channel": "nightly",
+  "version": "",
+  "metric": "FX_NEW_WINDOW_MS",
+  "useSubmissionDate": false,
+  "sanitize": true,
+  "trim": true,
+  "compare": "",
+  "sensibleCompare": true,
+  "evoVersions": "3",
+  "filters": {
+    "application": "Firefox"
+  }
+};
+
+const node = 'winOpen';
+
 class App extends Component {
+  componentDidMount() {
+    window.TelemetryWrapper.go(plot, document.getElementById(node));
+  }
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <h1 className="App-title">Welcome to React Telemetry demo</h1>
+          <div id={node}></div>
         </header>
       </div>
     );
