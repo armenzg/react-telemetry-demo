35e3cc273cea953b1f900e271fbc037d15457797
Armen Zambrano G <armenzg@gmail.com>
2017-10-10 15:00:00 -0400

    Trying to use telemetry-next-node

--- src/App.js
+++ src/App.js
@@ -1,6 +1,25 @@
 import React, { Component } from 'react';
+import Telemetry from 'telemetry-next-node';
 
 class App extends Component {
+  componentDidMount() {
+    // XXX: Unfortunately this fails with:
+    // TypeError: request.get(...).buffer is not a function
+    // Initialize library
+    Telemetry.init(() => {
+      const version = Telemetry.getVersions()[0]; // Get the first available version
+
+      // Optain a mapping from filter names to possible values of those filters
+      const parts = version.split("/");
+      Telemetry.getFilterOptions(parts[0], parts[1], (filters) => {
+        console.log("Measures available:");
+        filters.metric.forEach((measure) => {
+          console.log(measure);
+        });
+      });
+    });
+  }
+
   render() {
     return (
       <div className="App">
